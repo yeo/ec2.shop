ecffd429cd5df4a98341de6809d6d8c4af57208f
Vinh <vinh@axcoto.com>
2020-06-30 15:47:12 -0700

    Download previous generation

--- download.sh
+++ download.sh
@@ -7,4 +7,6 @@ ts=`date +%s`
 for region in `curl 'https://a0.p.awsstatic.com/pricing/1.0/ec2/manifest.json?timestamp=1593164829522'  | jq -r '.ec2[]'`; do
 
   curl "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/$region/ondemand/linux/index.json?timestamp=$ts" -o data/$region-ondemand.json
+
+  curl "https://a0.p.awsstatic.com/pricing/1.0/ec2/region/$region/previous-generation/ondemand/linux/index.json?timestamp=$ts" -o data/$region-ondemand-previous-generation.json
 done
