91e8e08dcc48221e918f0ad68fde82b6c5db5978
Vinh <vinh@mailwip.com>
2024-07-13 23:10:03 -0700

    download partial ri rds data

--- data/download_elasticache.rb
+++ data/download_elasticache.rb
@@ -28,82 +28,45 @@ regions=JSON.parse(HTTPX.get("https://b0.p.awsstatic.com/locations/1.0/aws/curre
 puts "found #{regions.length} regions"
 
 regions.each do |key, region|
-  puts "fetching #{region['name']}"
+  puts "[postgres] fetching #{region['name']}"
 
   [{
     name: 'rds-mysql-reservedinstance-multi-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-mysql-reservedinstance-multi-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Multi-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mysql-reservedinstance-multi-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mysql-reservedinstance-single-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-mysql-reservedinstance-single-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Single-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mysql-reservedinstance-single-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-
-  {
+  },{
     name: 'rds-mariadb-reservedinstance-multi-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-mariadb-reservedinstance-multi-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Multi-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mariadb-reservedinstance-multi-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mariadb-reservedinstance-single-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-mariadb-reservedinstance-single-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Single-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-mariadb-reservedinstance-single-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-
-  {
+  },{
     name: 'rds-postgresql-reservedinstance-multi-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-postgresql-reservedinstance-multi-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Multi-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-postgresql-reservedinstance-multi-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-postgresql-reservedinstance-single-az-1y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
-    name: 'rds-postgresql-reservedinstance-single-az-1y-partial',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Single-AZ/1%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },
-  {
+  }, {
     name: 'rds-postgresql-reservedinstance-single-az-3y',
     url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }].each do |instance_class|
+  },].each do |instance_class|
     puts "fetch #{instance_class[:name]} in region #{region['name']}"
 
     region_price_data = HTTPX.get(instance_class[:url])
