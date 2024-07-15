bdedf4bfd321756f149c77640d2c583de6a94dac
Vinh <vinh@mailwip.com>
2024-07-15 03:29:19 -0700

    add script to download es

---
+++ data/download_es.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby3
+
+require 'httpx'
+require 'json'
+require 'cgi'
+
+ts=Time.now.to_i
+
+[{
+  name: 'es-ondemand',
+  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/es/USD/current/es-ondemand.json?timestamp=#{ts}"
+}].each do |instance_class|
+  puts "#{instance_class[:name]} downloading..."
+
+  region_price_data = HTTPX.get(instance_class[:url])
+  File.write("data/es/#{instance_class[:name]}.json", region_price_data)
+end
+
+
+regions=JSON.parse(HTTPX.get("https://b0.p.awsstatic.com/locations/1.0/aws/current/locations.json?timestamp=#{ts}"))
+
+puts "found #{regions.length} regions"
+
+regions.each do |key, region|
+  rn = CGI.escape(region['name'])
+  rc = CGI.escape(region['code'])
+
+  puts "[elasticache] fetching #{region['name']}"
+
+  %w(1%20year 3%20year).each do |y|
+    %w(No%20Upfront Partial%20Upfront All%20Upfront).each do |p|
+      %w(General%20purpose Compute%20optimized Memory%20optimized Storage%20optimized OR1).each do |t|
+        url = "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/es/USD/current/es-reservedinstance/#{y}/#{p}/#{rn}/#{t}/index.json?timestamp=#{ts}"
+
+        puts "fetch #{url}"
+        region_price_data = HTTPX.get url
+
+        File.write("data/es/es-reservedinstance-#{y}-#{p}-#{rc}-#{t}.json", region_price_data)
+      end
+    end
+  end
+end
