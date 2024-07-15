939050fad93fcdb9f4356a420d746e21c82b433a
Vinh <vinh@mailwip.com>
2024-07-14 21:29:07 -0700

    fix script to download elasticache

--- data/download_elasticache.rb
+++ data/download_elasticache.rb
@@ -7,19 +7,13 @@ require 'cgi'
 ts=Time.now.to_i
 
 [{
-  name: 'rds-postgresql-ondemand',
-  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-ondemand.json?timestamp=#{ts}"
-}, {
-  name: 'rds-mariadb-ondemand',
-  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-ondemand.json?timestamp={ts}"
-},{
-  name: 'rds-mysql-ondemand',
-  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-ondemand.json?timestamp={ts}"
+  name: 'elasticache',
+  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/elasticache/USD/current/elasticache.json?timestamp=1720953273260?timestamp=#{ts}"
 }].each do |instance_class|
   puts "#{instance_class[:name]} downloading..."
 
   region_price_data = HTTPX.get(instance_class[:url])
-  File.write("data/rds/#{instance_class[:name]}.json", region_price_data)
+  File.write("data/elasticache/#{instance_class[:name]}.json", region_price_data)
 end
 
 
@@ -28,48 +22,19 @@ regions=JSON.parse(HTTPX.get("https://b0.p.awsstatic.com/locations/1.0/aws/curre
 puts "found #{regions.length} regions"
 
 regions.each do |key, region|
-  puts "[postgres] fetching #{region['name']}"
+  rn = CGI.escape(region['name'])
 
-  [{
-    name: 'rds-mysql-reservedinstance-multi-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mysql-reservedinstance-multi-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mysql-reservedinstance-single-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mysql-reservedinstance-single-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mysql-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },{
-    name: 'rds-mariadb-reservedinstance-multi-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mariadb-reservedinstance-multi-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mariadb-reservedinstance-single-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-mariadb-reservedinstance-single-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-mariadb-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },{
-    name: 'rds-postgresql-reservedinstance-multi-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Multi-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-postgresql-reservedinstance-multi-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Multi-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-postgresql-reservedinstance-single-az-1y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Single-AZ/1%20year/No%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  }, {
-    name: 'rds-postgresql-reservedinstance-single-az-3y',
-    url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/rds/USD/current/rds-postgresql-reservedinstance/Single-AZ/3%20year/Partial%20Upfront/#{CGI.escape(region['name'])}/index.json?timestamp=#{ts}"
-  },].each do |instance_class|
-    puts "fetch #{instance_class[:name]} in region #{region['name']}"
+  puts "[elasticache] fetching #{region['name']}"
 
-    region_price_data = HTTPX.get(instance_class[:url])
-    File.write("data/rds/#{region["code"]}-#{instance_class[:name]}.json", region_price_data)
+  %w(1%20year 3%20year).each do |y|
+    %w(No%20Upfront Partial%20Upfront).each do |p|
+      %w(Standard Network%20optimized Memory%20optimized).each do |t|
+        url = "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/elasticache/USD/current/elasticache-reservedinstance/#{y}/#{p}/#{rn}/#{t}/index.json?timestamp=#{ts}"
+        puts "fetch #{url}"
+        region_price_data = HTTPX.get url
+
+        File.write("data/elasticache/elasticache-reservedinstance-#{y}-#{p}-#{rn}-#{t}.json", region_price_data)
+      end
+    end
   end
 end
