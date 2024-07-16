2e5b162bcccf1316b3ae58d071997645f9fbcb34
Vinh <vinh@mailwip.com>
2024-07-15 22:50:52 -0700

    msk data

---
+++ data/download_msk.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby3
+
+require 'httpx'
+require 'json'
+require 'cgi'
+
+ts=Time.now.to_i
+`mkdir -p data/msk`
+
+[{
+  name: 'msk',
+  url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/msk/USD/current/msk.json?timestamp=#{ts}"
+}].each do |instance_class|
+  puts "#{instance_class[:name]} downloading..."
+
+  region_price_data = HTTPX.get(instance_class[:url])
+  File.write("data/msk/#{instance_class[:name]}.json", region_price_data)
+end
