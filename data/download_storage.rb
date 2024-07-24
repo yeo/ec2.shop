ebe73a91b7fc81c16de00124479250999ea31d42
Vinh <vinh@mailwip.com>
2024-07-23 17:52:33 -0700

    add efs downloader

--- data/download_storage.rb
+++ data/download_storage.rb
@@ -6,12 +6,13 @@ require 'cgi'
 
 ts=Time.now.to_i
 `mkdir -p data/ebs`
+`mkdir -p data/efs`
 
 [{
   name: 'ebs',
   url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/ec2/USD/current/ebs.json?timestamp=#{ts}",
 
-  name 'efs',
+  name: 'efs',
   url: "https://b0.p.awsstatic.com/pricing/2.0/meteredUnitMaps/efs/USD/current/efs.json?timestamp=#{ts}"
 },].each do |instance_class|
   puts "#{instance_class[:name]} downloading..."
